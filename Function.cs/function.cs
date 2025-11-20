e4cb071d19cf500297ad93df88c68f516e1034bc
Gulika77777 <dachitazo232@gmail.com>
2025-11-20 17:30:36 +0400

    Add project files.

---
+++ Function.cs/function.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace Student_Manager.Student.cs
+
+namespace Student_Manager.Function.cs
+{
+    internal class Function
+    {
+       private List<Student> students = [];
+
+        public bool CreateNewStudent(string name,
+                                     int rollNumber,
+                                     char grade )
+        {
+
+            if (students.Exists(c => c.Name == name))
+            {
+                throw new ArgumentException("this student is registered");
+            }
+
+           students.Add(new Student(name, rollNumber, grade));
+
+
+            XmlSerializer serializer = new(typeof(List<Student>));
+
+            using (FileStream fs = new FileStream("students.xml", FileMode.Create))
+            {
+                serializer.Serialize(fs, students);
+            }
+            return true;
+        }
+
+    }
+}
---
+++ Student.cs/student.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace Student_Manager.Student.cs
+{
+    internal class Student
+    {
+        private char _grade;
+        public string Name { get; set; }
+
+        public int RollNumber { get; set; }
+        public char Grade { 
+            get 
+            {
+                return _grade;
+            }
+            set
+            {
+                if (value < 'A' || value > 'F')
+                {
+                    throw new ArgumentException("Grade must be between A and F.");
+                }
+            }
+
+        }
+
+
+        public Student(string name, int rollnumber, char grade)
+        {
+            this.Name = name;
+            this.RollNumber = rollnumber;
+            this.Grade = grade;
+        }
+
+    }
+
+}
